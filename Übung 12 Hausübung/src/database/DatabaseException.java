bec3aa10e2be1433a0a62b08bc2473b2e2d23591
LouisK112 <Louis.Krueger@web.de>
2016-02-12 21:15:28 +0100

    Kommentar in database

--- Übung 12 Hausübung/src/database/DatabaseException.java
+++ Übung 12 Hausübung/src/database/DatabaseException.java
@@ -11,6 +11,7 @@ public class DatabaseException extends Exception{
 
 	/**
 	 * Oberklasse für Exceptions
+	 * hier wurde ein kommentar eingefügt
 	 */
 	public DatabaseException(String fehler)  {
 		super(fehler);
