4a76daeae939ae831c148fe27a33e9bb7cf312a4
LouisK112 <Louis.Krueger@web.de>
2016-02-12 21:02:08 +0100

    weiterer Kommentar hinzugefügt

--- Übung 12 Hausübung/src/service/NewsletterRegistrationTest.java
+++ Übung 12 Hausübung/src/service/NewsletterRegistrationTest.java
@@ -6,6 +6,11 @@ import org.junit.Test;
 
 public class NewsletterRegistrationTest {
 
+	/**
+	 * Hier wurde etwas hinzugefügt
+	 * Hier wurde noch viel mehr gemacht
+	 * 
+	 */
 	@Test
 	public void test() {
 		fail("Not yet implemented");
