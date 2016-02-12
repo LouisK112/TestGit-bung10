e94db199d39b6424a954ba7498da449660dfc0b9
LouisK112 <Louis.Krueger@web.de>
2016-02-12 21:21:54 +0100

    DatabaseException neuer Kemmentar + Konsolenausgabe

--- Übung 12 Hausübung/src/database/DatabaseException.java
+++ Übung 12 Hausübung/src/database/DatabaseException.java
@@ -12,9 +12,11 @@ public class DatabaseException extends Exception{
 	/**
 	 * Oberklasse für Exceptions
 	 * hier wurde ein kommentar eingefügt
+	 * das hier ist der nächste Kommentar 
 	 */
 	public DatabaseException(String fehler)  {
 		super(fehler);
+		System.out.println("Haha");
 	
 		
 		
